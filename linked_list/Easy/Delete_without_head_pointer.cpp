8b43bfba6d19c3f12b56c4a3000cf234b67927a4
Ismail Choukri <ichoukri@e2r2p3.1337.ma>
2022-01-31 12:31:31 +0100

    Delete_without_head_pointer

---
+++ linked_list/Easy/Delete_without_head_pointer.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+void deleteNode(Node *del)
+{
+	Node *next_node;
+	next_node = del->next;
+	del->data = next_node->data;
+	del->next = next_node->next;
+	free(next_node);
+}
